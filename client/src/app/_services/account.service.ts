cd531f8a38b834826c480f1350ec45564c07269c
zion66682 <zion82@gmail.com>
2025-02-22 22:48:42 +1300

    End of section 9

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -2,19 +2,20 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable, signal } from '@angular/core';
 import { User } from '../_models/user';
 import { map } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AccountService {
   private http = inject(HttpClient);
-  baseUrl = 'https://localhost:5001/api/';
+  baseUrl = environment.apiUrl;
   currentUser = signal<User | null>(null);
 
-  login(model: any){
+  login(model: any) {
     return this.http.post<User>(this.baseUrl + 'account/login', model).pipe(
-      map(user => {
-        if (user){
+      map((user) => {
+        if (user) {
           localStorage.setItem('user', JSON.stringify(user));
           this.currentUser.set(user);
         }
@@ -22,10 +23,10 @@ export class AccountService {
     );
   }
 
-  register(model: any){
+  register(model: any) {
     return this.http.post<User>(this.baseUrl + 'account/register', model).pipe(
-      map(user => {
-        if (user){
+      map((user) => {
+        if (user) {
           localStorage.setItem('user', JSON.stringify(user));
           this.currentUser.set(user);
         }
@@ -39,4 +40,3 @@ export class AccountService {
     this.currentUser.set(null);
   }
 }
-
